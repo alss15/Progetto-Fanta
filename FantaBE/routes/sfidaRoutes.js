29c00e22f4982463a7afdb0865ceffed56a6f43c
Alessia <alessia.spampinato15895@gmail.com>
2025-04-08 16:55:06 +0200

    test postman

--- FantaBE/routes/sfidaRoutes.js
+++ FantaBE/routes/sfidaRoutes.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const sfidaController = require ('../controllers/sfidaController');
 const authMiddleware = require('../middleware/authMiddleware');
